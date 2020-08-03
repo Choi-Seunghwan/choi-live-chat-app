dc989172d054f6892e60f66c1de7f588b7ce1435
Choi-Seunghwan <choiseunghwan.tech@gmail.com>
2020-08-03 19:29:24 +0900

    prettier, vscode 설정 및 router 업데이트

--- src/pages/LiveRoom.vue
+++ src/pages/LiveRoom.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>
-    LiveRoom!
-
+  <div class="liveRoom">
+    <div class="liveRoom__top"></div>
+    <VideoPlayer />
+    <div class="liveRoom__bottom"></div>
   </div>
 </template>
 <script>
-export default {};
+import VideoPlayer from "../components/videoPlayer/VideoPlayer";
+export default {
+  components: {
+    VideoPlayer
+  },
+  mounted() {}
+};
 </script>
 <style lang="scss"></style>
