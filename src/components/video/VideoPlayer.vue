6954d73e7f8fe891908c92e2f092add4136b4315
Choi-Seunghwan <choiseunghwan.tech@gmail.com>
2020-08-11 09:27:48 +0900

    video 공부 중

--- src/components/video/VideoPlayer.vue
+++ src/components/video/VideoPlayer.vue
@@ -1,11 +1,34 @@
 <template>
-  <div class="videoPlayer"></div>
+  <div class="videoPlayer">
+    <h4>remote Video</h4>
+    <video autoplay playsinline ref="remoteVideo" id="remoteVideo" width="300" height="200"></video>
+    <p />
+    <hr />
+    <h4>local Video</h4>
+    <video autoplay playsinline ref="localVideo" id="localVideo" width="300" height="200"></video>
+  </div>
 </template>
 <script>
 export default {
   name: 'videoPlayer',
+  data() {
+    return {
+      localVideoEl: undefined,
+      remoteVideoEl: undefined,
+      pc: undefined
+    };
+  },
   props: {
     msg: String
+  },
+  mounted() {
+    this.localVideoEl = this.$refs['localVideo'];
+    this.remoteVideoEl = this.$refs['remoteVideo'];
+  },
+  methods: {
+    createPeerConnection() {
+      this.pc = new RTCPeerConnection();
+    }
   }
 };
 </script>
