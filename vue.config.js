9f40f2507689878673e52129f74698af165b034c
Choi-Seunghwan <choiseunghwan.tech@gmail.com>
2020-08-15 18:08:04 +0900

    multiple pages build

--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,21 @@ module.exports = {
         '@': path.join(__dirname, 'src/')
       }
     }
+  },
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.js',
+      // the source template
+      template: 'public/app/index.html',
+      // output as dist/app/index.html
+      filename: 'app/index.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'App Index Page',
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    }
   }
 };
