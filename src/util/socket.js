ffa2df7213c0522addd653a6abdfd898848dbcb4
Choi-Seunghwan <choiseunghwan.tech@gmail.com>
2020-12-04 21:04:35 +0900

    소켓 구조화

--- src/util/socket.js
+++ src/util/socket.js
@@ -1,9 +1,29 @@
 import io from 'socket.io-client';
 
 export default {
-  socket: undefined,
+  ws: undefined,
+  connectionStatus: 1,
+  connectionStatuses: {
+    IDLE: 1,
+    CONNECTING: 2,
+    CONNECTED: 3,
+    DISCONNECTED: 4,
+    WAITING: 5
+  },
 
   connection() {
-    this.socket = io('localhost:5000');
+    this.ws = io('localhost:5000');
+  },
+
+  sendMessage(method, param) {
+    if (!this.ws) return false;
+
+    this.ws.emit(method, param);
+  },
+
+  callUser(offer) {
+    this.ws.emit('call-user', {
+      offer
+    });
   }
 };
