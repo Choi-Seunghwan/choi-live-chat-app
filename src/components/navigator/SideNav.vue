e47763e0002c57348efc7e42bdaef733398ff09a
Choi-Seunghwan <choiseunghwan.tech@gmail.com>
2020-12-08 12:03:01 +0900

    SideNav 아이템, 스타일 작업

--- src/components/navigator/SideNav.vue
+++ src/components/navigator/SideNav.vue
@@ -1,5 +1,20 @@
 <template>
-  <aside v-show="showSideNav" class="sideNav"></aside>
+  <aside v-show="showSideNav" class="sideNav">
+    <nav class="nav">
+      <ul>
+        <li>
+          <router-link :to="{ path: '/' }" class="nav__item">{{ $t('home') }}</router-link>
+        </li>
+        <li>
+          <router-link :to="{ path: '/' }" class="nav__item">{{ $t('live') }}</router-link>
+        </li>
+        <li>
+          <router-link :to="{ path: '/' }" class="nav__item">{{ $t('startLive') }}</router-link>
+        </li>
+      </ul>
+      <div class="divide" />
+    </nav>
+  </aside>
 </template>
 
 <script>
@@ -20,5 +35,33 @@ export default {
   width: 240px;
   height: 100%;
   background-color: #f7f7f7;
+
+  .divide {
+    height: 1px;
+    background-color: #e6e6e6;
+  }
+
+  .nav {
+    width: 100%;
+    height: 100%;
+    margin-top: 56px;
+
+    ul {
+      padding: 0;
+      margin: 0;
+    }
+
+    li {
+      list-style: none;
+    }
+
+    &__item {
+      display: block;
+      padding: 15px;
+      font-size: 14px;
+      line-height: 20px;
+      color: #4d4d4d;
+    }
+  }
 }
 </style>
