07436bdeede81d82582b4e24cd04f892420921eb
Choi-Seunghwan <choiseunghwan.tech@gmail.com>
2020-12-08 16:34:10 +0900

    StartLiveInterface 컴포넌트 생성

---
+++ src/components/live/StartLiveInterface.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="StartLiveInterface">
+    <div class="liveType">
+      <h4 class="liveType__text">
+        {{ $t('liveType') }}
+      </h4>
+      <div class="liveType__inputs">
+        <button class="input">
+          {{ $t('broadcast') }}
+        </button>
+        <button class="input">
+          {{ $t('radio') }}
+        </button>
+      </div>
+    </div>
+    <div class="liveForm">
+      <div class="title">
+        <h4 class="title__text">{{ $t('title') }}</h4>
+        <input :placeholder="$t('title')" class="title__input" />
+      </div>
+    </div>
+    <div class="interface">
+      <button>{{ $t('startLive') }}</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.StartLiveInterface {
+  .liveType {
+    &__inputs {
+      display: flex;
+      justify-content: center;
+
+      .input {
+        margin: 0 10px;
+      }
+    }
+  }
+
+  .liveForm {
+    .title {
+      &__text {
+      }
+
+      &__input {
+        width: 100%;
+      }
+    }
+  }
+
+  .interface {
+    margin-top: 15px;
+    text-align: center;
+  }
+}
+</style>
--- src/pages/StartLive.vue
+++ src/pages/StartLive.vue
@@ -1,32 +1,16 @@
 <template>
   <div class="startLive">
-    <div class="liveType">
-      <h4 class="liveType__text">
-        {{ $t('liveType') }}
-      </h4>
-      <div class="liveType__inputs">
-        <button class="input">
-          {{ $t('broadcast') }}
-        </button>
-        <button class="input">
-          {{ $t('radio') }}
-        </button>
-      </div>
-    </div>
-    <div class="liveForm">
-      <div class="title">
-        <h4 class="title__text">{{ $t('title') }}</h4>
-        <input :placeholder="$t('title')" class="title__input" />
-      </div>
-    </div>
-    <div class="interface">
-      <button>{{ $t('startLive') }}</button>
-    </div>
+    <StartLiveInterface />
   </div>
 </template>
 
 <script>
-export default {};
+import StartLiveInterface from '@/components/live/StartLiveInterface.vue';
+export default {
+  components: {
+    StartLiveInterface
+  }
+};
 </script>
 
 <style lang="scss">
@@ -36,32 +20,5 @@ export default {};
   left: 50%;
   transform: translate(-50%, -50%);
   min-width: 320px;
-
-  .liveType {
-    &__inputs {
-      display: flex;
-      justify-content: center;
-
-      .input {
-        margin: 0 10px;
-      }
-    }
-  }
-
-  .liveForm {
-    .title {
-      &__text {
-      }
-
-      &__input {
-        width: 100%;
-      }
-    }
-  }
-
-  .interface {
-    margin-top: 15px;
-    text-align: center;
-  }
 }
 </style>
