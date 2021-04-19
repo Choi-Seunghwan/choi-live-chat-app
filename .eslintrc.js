d5e6a624e1b9b0be5f8c086021ef3c1f68c18adf
Choi-Seunghwan <choiseunghwan.tech@gmail.com>
2021-04-19 23:35:34 +0900

    [VF] frontend typescript 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,14 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: '@typescript-eslint/parser'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-unused-vars': 'off'
-  }
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
+  },
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier', '@vue/typescript']
 };
